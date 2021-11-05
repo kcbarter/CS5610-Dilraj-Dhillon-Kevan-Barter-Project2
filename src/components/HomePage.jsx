3d6f5bc977bfce5020c5a33a5f859a4406449536
Kevan <kcbarter@msn.com>
2021-11-05 14:07:17 -0700

    Buttons added and centered on the screen

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "../css/Button.css";
 import {Link, NavLink} from "react-router-dom";
+import "../css/HomePage.css";
 
 
 export default function HomePage(){
     return(
-        <div class="homePageContiner">
+        <div class="homePageContainer">
             <Link exact to="/free_play"> 
                 <button class="linkButton">Free Play</button>
             </Link>
