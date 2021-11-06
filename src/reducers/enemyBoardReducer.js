43d9ffd7f3479c666c289a36328c3ca1c50afeca
DilrajDhillon <dhillon.d@husky.neu.edu>
2021-11-06 14:31:06 -0700

    added random horizontal logic for default state

--- src/reducers/enemyBoardReducer.js
+++ src/reducers/enemyBoardReducer.js
@@ -1,16 +1,42 @@
+const shipLengths = [5, 4, 3, 3, 2];
+
 const defaultState = [
-    ['empty', 'ship', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'ship', 'ship'],
-    ['empty', 'ship', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
-    ['empty', 'ship', 'empty', 'empty', 'ship', 'empty', 'empty', 'empty', 'empty', 'empty'],
-    ['empty', 'ship', 'empty', 'empty', 'ship', 'empty', 'ship', 'empty', 'empty', 'empty'],
-    ['empty', 'ship', 'empty', 'empty', 'ship', 'empty', 'ship', 'empty', 'empty', 'empty'],
-    ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'ship', 'empty', 'empty', 'empty'],
     ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
     ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
-    ['empty', 'empty', 'empty', 'empty', 'empty', 'ship', 'ship', 'ship', 'ship', 'empty'],
+    ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
+    ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
+    ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
+    ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
+    ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
+    ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
+    ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
     ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
 ];
 
+for (let shipLength of shipLengths) {
+    let row = Math.floor(Math.random() * 10);
+    let col = Math.floor(Math.random() * 10);
+    const BOUNDARY = 10;
+    let boundaryFlag = 1;
+    while (boundaryFlag === 1) {
+        boundaryFlag = 0;
+        for (let i = col; i < col + shipLength; i++) {
+            if (i === BOUNDARY || defaultState[row][i] === 'ship') {
+                boundaryFlag = 1;
+                break;
+            }
+        }
+        if (boundaryFlag === 0) {
+            break;
+        }
+        row = Math.floor(Math.random() * 10);
+        col = Math.floor(Math.random() * 10);
+    }
+
+    defaultState[row].fill('ship', col, col + shipLength);
+}
+
+// TO DO: deep copy
 const initialState = [
     ['empty', 'ship', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'ship', 'ship'],
     ['empty', 'ship', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
